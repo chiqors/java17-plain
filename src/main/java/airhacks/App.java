1894f5004461897a8471f171a27a9acf4c80971a
chiqors <bandi_nada@live.com>
2022-10-20 23:06:49 +0700

    feat: add test function for testing purpose

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -8,6 +8,13 @@ package airhacks;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("is working");
+        String content = """
+            Hi there!\r
+            First of all, welcome to Repository. If you have any issues, please let us know so that i can help you! \r
+            \r
+            Check out our:\r
+            Main page: https://airhacks.com\r
+            """;
+        System.out.println(content);
     }
 }
